793f7e6f9898d03657cd1c69d381dedf46accf32
Franco Pellegrini <frapell@gmail.com>
2011-12-21 14:00:54 -0300

    Agregado portlet con link a 'Agregar noticia'

---
+++ src/telesur/portlets/addnews.py
@@ -0,0 +1,66 @@
+from zope.interface import implements
+
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.app.portlets.portlets import base
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from telesur.portlets import _
+
+
+class IAddNews(IPortletDataProvider):
+    """A portlet
+
+    It inherits from IPortletDataProvider because for this portlet, the
+    data that is being rendered and the portlet assignment itself are the
+    same.
+    """
+
+    # TODO: Add any zope.schema fields here to capture portlet configuration
+    # information. Alternatively, if there are no settings, leave this as an
+    # empty interface - see also notes around the add form and edit form
+    # below.
+
+    # some_field = schema.TextLine(title=_(u"Some field"),
+    #                              description=_(u"A field to use"),
+    #                              required=True)
+
+
+class Assignment(base.Assignment):
+    """Portlet assignment.
+
+    This is what is actually managed through the portlets UI and associated
+    with columns.
+    """
+
+    implements(IAddNews)
+
+    def __init__(self):
+        pass
+
+
+    @property
+    def title(self):
+        """This property is used to give the title of the portlet in the
+        "manage portlets" screen.
+        """
+        return _(u"Add news")
+
+
+class Renderer(base.Renderer):
+    """Portlet renderer.
+
+    This is registered in configure.zcml. The referenced page template is
+    rendered, and the implicit variable 'view' will refer to an instance
+    of this class. Other methods can be added and referenced in the template.
+    """
+
+    render = ViewPageTemplateFile('addnews.pt')
+
+class AddForm(base.NullAddForm):
+    """Portlet add form.
+    """
+    def create(self):
+        return Assignment()
+
+    
