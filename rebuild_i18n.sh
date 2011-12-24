02956ab665aa82b041e1507677651e921687c8d6
hvelarde <hector.velarde@gmail.com>
2011-12-24 09:22:03 -0600

    actualización de i18n

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -4,11 +4,11 @@ I18NDOMAIN="telesur.portlets"
 
 # Synchronise the templates and scripts with the .pot.
 # All on one line normally:
-i18ndude rebuild-pot --pot src/telesur/portlets/locales/${I18NDOMAIN}.pot \
+bin/i18ndude rebuild-pot --pot src/telesur/portlets/locales/${I18NDOMAIN}.pot \
     --create ${I18NDOMAIN} \
-   .
+    src/telesur/portlets
 
 # Synchronise the resulting .pot with all .po files
 for po in src/telesur/portlets/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
-    i18ndude sync --pot src/telesur/portlets/locales/${I18NDOMAIN}.pot $po
+    bin/i18ndude sync --pot src/telesur/portlets/locales/${I18NDOMAIN}.pot $po
 done
