acd160e7589693ee44bef8a60de71a23b30274d4
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2012-01-28 05:19:15 -0430

    Cambiar el long_description agregar más classifiers y la url del paquete

--- setup.py
+++ setup.py
@@ -6,18 +6,20 @@ version = '1.0'
 setup(name='telesur.portlets',
       version=version,
       description="Aqui se crearan portlets a utilizar en el sitio de TeleSUR",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
         ],
-      keywords='',
+      keywords='portlets website plone teleSUR',
       author='',
       author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      url='https://github.com/desarrollotv/telesur.portlets',
       license='GPL',
       packages=find_packages('src'),
       package_dir={'': 'src'},
